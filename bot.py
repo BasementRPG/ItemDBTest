a89b97e58406d1271b1d13a0b064fea3756ad186
BasementRPG <107715049+BasementRPG@users.noreply.github.com>
2026-09-20 11:29:30 -0400

    Fix ORDER BY clause syntax in SQL query

--- bot.py
+++ bot.py
@@ -5302,7 +5302,7 @@ async def run_update_db(
                     npc_level,
                     guild_id
                 FROM item_database
-                ORDER BY ASC()
+                ORDER BY item_name ASC
                 LIMIT 20
                 """
             )
